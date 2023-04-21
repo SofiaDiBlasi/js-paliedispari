3fbdfa43842570db8d9f7512c529fe3503bf0357
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-04-21 15:47:17 +0100

    fine parte esercizio parola palindroma

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@
 let parola = prompt("Inserisci qui una parola palindroma");
 
 //facciamo un funzione per verificare se la parola è palindroma
-function checkPalindromo(parola) {
+function checkPalindromo(parolaDaVerificare) {
      
     //trasformo parola in un array di caratteri
-    let arrayParola = Array.from(parola);
+    let arrayParola = Array.from(parolaDaVerificare);
     //faccio in modo che si ribalti
     let arrayRibaltato = arrayParola.reverse();
     //facciamo in modo che i caratteri dell'arrei invertito formino nuovamente una stringa
-    let parolaInvertita = arrayRibaltato.join();
+    let parolaInvertita = arrayRibaltato.join("");
+
+    if (parolaDaVerificare == parolaInvertita){
+        console.log("La parola è ok");
+    } else {
+        console.log("La parola non è ok");
+    }
 }
+
+checkPalindromo(parola);
